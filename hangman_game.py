7b18af187ba8ddb2e32a6d627bf0addd8d3fda16
glennsvel90 <glennselwynvel@gmail.com>
2016-08-22 03:47:46 -0400

    better hangman

--- hangman_game.py
+++ hangman_game.py
@@ -2,8 +2,9 @@ import os
 import random
 import sys
 
+
 #make a list of words
-words=["samantha",
+words = ["samantha",
 "sarah",
 "sophie",
 "sariah","sunny",
@@ -17,37 +18,33 @@ words=["samantha",
 "sophia",
 "selena"]
 
+
 def clear():
-	if os.name=='nt':
+	if os.name == 'nt':
 		os.system('cls')
 	else:
 		os.system('clear')
 
 
-
 def draw(wrong_guesses,right_guesses,random_word):
 	clear()
-
 	print('Strikes: {}/7'.format(len(wrong_guesses)))
 	print ('')
-
 	for letter in wrong_guesses:
 		print(letter,)
 	print('\n\n')
-
 	for letter in random_word:
 		if letter in right_guesses:
-			print (letter,)
+			print (letter, end = '')
 		else:
-			print("_",)
-
+			print("_", end = '')
 	print('')
+
 
 def get_guess(wrong_guesses,right_guesses):
 	while True:
-		guess= raw_input("Guess a letter: ").lower()
-
-		if len(guess)!=1:
+		guess= input("Guess a letter: ").lower()
+		if len(guess) != 1:
 			print ("You only can guess 1 letter at a time")
 		elif guess in wrong_guesses or guess in right_guesses:
 			print ("You guessed that letter already before")
@@ -56,54 +53,53 @@ def get_guess(wrong_guesses,right_guesses):
 		else:
 			return guess
 
+
 def play(done):
 	clear()
-	random_word=random.choice(words)
-	wrong_guesses=[]
-	right_guesses=[]
+	random_word = random.choice(words)
+	wrong_guesses = []
+	right_guesses = []
 
 	while True:
-		draw(wrong_guesses,right_guesses,random_word)
-		guess=get_guess(wrong_guesses,right_guesses)
-
+		draw(wrong_guesses, right_guesses, random_word)
+		guess = get_guess(wrong_guesses, right_guesses)
 		if guess in random_word:
 			right_guesses.append(guess)
-			found=True
+			found = True
 			for letter in random_word:
 				if letter not in right_guesses:
-					found=False
+					found = False
 			if found:
 				print ("You Win!!! CONGRATULATIONS!!!")
 				print("The secret word was {}".format(random_word))
-				done=True
+				done = True
 		else:
 			wrong_guesses.append(guess)
-			if len(wrong_guesses)==7:
-				draw(wrong_guesses,right_guesses,random_word)
+			if len(wrong_guesses) == 7:
+				draw(wrong_guesses, right_guesses, random_word)
 				print ("You Lost!  T_T")
 				print ("The secret word was {}".format(random_word))
-				done=True
+				done = True
 
 		if done:
-			play_again=raw_input("Wanna Play Again? Y/n").lower()
-			if play_again!= "n":
-				return play(done=False)
+			play_again = input("Wanna Play Again? Y/n").lower()
+			if play_again != "n":
+				return play(done = False)
 			else:
 				sys.exit()
 
 
 def welcome():
-	start= raw_input("Press enter/return to start, or enter Q to quit ").lower()
+	start = input("Press enter/return to start, or enter Q to quit ").lower()
 	if start == "q":
 		print ("Bye Bye! Hope you come back and play!")
 		sys.exit()
 	else:
 		return True
 
+
 print("Welcome to the Hangman Game!! ")
-
-done=False
-
+done = False
 while True:
 	clear()
 	welcome()
